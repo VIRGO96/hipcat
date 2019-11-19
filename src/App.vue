149b7ca55cb692c6d1588b55ab14c10895b58fdd
VIRGO96 <dawoodahmed696@gmail.com>
2019-11-19 10:53:31 +0500

    css classes in App vue

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,24 @@
 </template>
 
 <script>
-  export default {}
+  export default {
+    created(){
+        localStorage.setItem("options","dashboard")
+    }
+  }
 </script>
 <style lang="scss">
+
+h5{
+  font-weight:bold !important;
+}
 label{
   color: #6C6C6C !important;
+  font-weight:bold !important
 }
-// body{
-//   background-color:white !important;
-// }
+body{
+  background-color:white !important;
+}
 .content{
   padding: 0px 15px !important;
 
@@ -24,9 +33,28 @@ label{
   padding: 0px 15px !important;
 
 }
+#modal-8___BV_modal_content_ {
+    margin: -20vh auto 0px auto !important 
+}
+#modal-7___BV_modal_content_ {
+    margin: -20vh auto 0px auto !important 
+}
 .familia{
   font-family: Monteserrat;
 }
+/* Smartphones (portrait and landscape) ----------- */
+@media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
+.button-margins {
+margin-bottom:1rem;
+}
+.button-margins2 {
+margin-bottom:1.2rem !important;
+}
+.left-aligns{
+  text-align:center !important;
+}
+}
+
   .vue-notifyjs.notifications{
     .list-move {
       transition: transform 0.3s, opacity 0.4s;
